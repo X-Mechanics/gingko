988f4c7ba18f4255a5d03bf556ac6f122feb2dda
Michael Park <q1kollab@gmail.com>
2021-04-01 01:26:52 -0400

    setuptools requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
+import os
 
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(
-        name='gingko',
-        version='0.0',
-        packages=find_packages(),
-    )
+    name='gingko', 
+    install_requires=install_requires,
+    version='0.1',
+    packages=find_packages()
+)
